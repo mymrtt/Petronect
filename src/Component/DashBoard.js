25052e91ab394d3fe8fe1b9f88bf5fcecf052bec
Kelvin Almeida <kelvin.f.almeida@gmail.com>
2019-09-19 09:24:41 -0300

    developing SideBar

---
+++ src/Component/DashBoard.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+// Components
+import SideBar from './SideBar';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  background: transparent linear-gradient(180deg, #115680 0%, #116EA0 100%) 0% 0% no-repeat padding-box;
+`;
+
+
+class DashBoard extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+ 
+
+  render() {
+    return (
+      <Container>
+          <SideBar/>
+      </Container>
+   );}
+  }
+
+
+export default DashBoard;
