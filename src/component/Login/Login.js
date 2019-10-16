ba94a6cbdad4e75c2939cb79bac4bd10f8f2c0fd
Cicero Matos <ciceropzr@gmail.com>
2019-10-16 16:05:35 -0300

    Feature Create Account

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 // Images
 import logo from '../../assets/img/LogoPNE.png';
+import logoW from '../../assets/img/logoBranca.svg';
 import showPassword from '../../assets/icon/login-show-password.svg';
 import hidePassword from '../../assets/icon/login-hide-password.svg';
 import imagemPrincpal from '../../assets/img/Grupo-8105.svg';
@@ -11,7 +12,7 @@ import imagemPrincpal from '../../assets/img/Grupo-8105.svg';
 // Redux
 import { loginUserThunk } from '../../dataflow/thunks/login-thunk';
 
-import  { updateError } from '../../dataflow/modules/login-module';
+import { updateError } from '../../dataflow/modules/login-module';
 
 const mapStateToProps = (state) => ({
 	error: state.login.error,
@@ -69,10 +70,16 @@ const InputContainer = styled.form`
 
 const Logo = styled.img`
 	width: 35%;
-	padding: 1rem 0;
+	margin: 1rem 0;
 	@media (max-width: 450px) {
 		width: 55%;
 	}
+`;
+
+const LogoCreate = styled.img`
+	position: absolute;
+	top: 10%;
+	width: 10vw;
 `;
 
 const InputBox = styled.span`
@@ -80,7 +87,7 @@ const InputBox = styled.span`
 	display: flex;
 	flex-direction: ${(props) => (props.alt ? 'row' : 'column')};
 	justify-content: ${(props) => props.alt && 'space-between'};
-	width: 55%;
+	width: ${(props) => props.width};
 	margin-top: ${(props) => props.last && '.5rem'};
 	@media (max-width: 768px) {
 		width: 60%;
@@ -91,8 +98,8 @@ const InputBox = styled.span`
 `;
 
 const Label = styled.label`
-	font: 500 12px Eurostile, sans-serif;
-	margin: 0 0 .25rem .5rem;
+	font: 500 12px Eurostile;
+	margin: 0 0 .25rem 1rem;
 	letter-spacing: 0;
 	color: #7FBA4C;
 `;
@@ -101,6 +108,7 @@ const Input = styled.input`
 	width: 100%;
 	height: 3rem;
 	padding: 1rem;
+	padding-left: calc(1rem - 2px);
 	font-size: 1rem;
 	background: #FAFAFA 0% 0% no-repeat padding-box;
 	border: 1px solid ${(props) => (props.error ? '#D53B40' : '#7FBA4C')};
@@ -108,7 +116,7 @@ const Input = styled.input`
 	outline: none;
 
 	::placeholder {
-		font: Light 16px Open Sans;
+		font: 300 16px Open Sans, sans serif;
 		letter-spacing: 0;
 		color: #959595;
 	}
@@ -124,7 +132,7 @@ const IconInputPassword = styled.img`
 `;
 
 const Button = styled.button`
-	width: 55%;
+	width: ${(props) => props.width};
 	height: 3rem;
 	margin-top: 2.5rem;
 	background: #115680;
@@ -133,7 +141,7 @@ const Button = styled.button`
 	border-radius: 4px;
 	outline: none;
 	text-align: center;
-	font: 500 1rem eurostile, sans serif;
+	font: 600 1rem eurostile, sans serif;
 	letter-spacing: 0;
 	color: #FAFAFA;
 
@@ -166,6 +174,7 @@ const Link = styled.p`
 	letter-spacing: 0;
 	color: ${(props) => props.color || '#505050'} ;
 	text-decoration: ${(props) => (props.color ? 'underline' : 'none')} ;
+	cursor: pointer;
 `;
 
 const LoginBox = styled.div`
@@ -234,10 +243,57 @@ const TextError = styled.p`
 	}
 `;
 
+const CreateContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
+	width: 100%;
+`;
+
+const CreateBox = styled.form`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 30%;
+	min-width: 360px;
+	padding: 3rem 4rem;
+	background: #FFF;
+	box-shadow: 0px 1px 2px #0000001A;
+	border-radius: 4px;
+	transition: width 1s; 
+
+	@media(max-width: 1440px) {
+		width: 50%;
+	}
+`;
+
+const TermsText = styled.p`
+	width: 90%;
+	margin: 1.5rem 0 -1rem;
+	font: 400 14px Eurostile;
+	letter-spacing: 0;
+	color: #505050;
+
+	strong {
+		text-decoration: underline;
+	}
+`;
+
+
+const CreateTitle = styled.h1`
+	align-self: flex-start;
+	margin: 0 1rem 1rem;
+	font: 700 24px Eurostile;
+	letter-spacing: 0;
+	color: #116EA0;
+`;
+
 class Login extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			currentScreen: 'create',
 			showPassword: true,
 		};
 	}
@@ -248,7 +304,7 @@ class Login extends Component {
 		}));
 	}
 
-	handleSubmit = (ev) => {
+	loginSubmit = (ev) => {
 		ev.preventDefault();
 		this.props.loginUserThunk({
 			email: this.loginEmailRef.value.toLowerCase(),
@@ -277,57 +333,149 @@ class Login extends Component {
 		return null;
 	}
 
+	handleCreate = () => {
+		if (this.state.currentScreen !== 'create') {
+			this.setState({
+				currentScreen: 'create',
+			});
+		}
+	}
+
+	createSubmit = (ev) => {
+		ev.preventDefault();
+		console.log('nome: ', this.createNameRef.value);
+		console.log('email: ', this.createEmailRef.value);
+		console.log('senha: ', this.createPasswordRef.value);
+	}
+
+	renderLogin = () => <>
+		<InputContainer onSubmit={this.loginSubmit}>
+			<Logo src={logo} />
+			<InputBox width='55%'>
+				<Label>E-mail</Label>
+				<Input
+					ref={(node) => { this.loginEmailRef = node; }}
+					required
+					autoFocus
+					type='email'
+					placeholder={'Digite sua senha e-mail'}
+					error={this.props.error}
+					onChange={this.handleError}
+				/>
+			</InputBox>
+			<InputBox last width='55%'>
+				<Label>Senha</Label>
+				<Input
+					ref={(node) => { this.loginPasswordRef = node; }}
+					type={this.state.showPassword ? 'password' : 'text'}
+					required
+					placeholder={'Digite sua senha'}
+					error={this.props.error}
+					onChange={this.handleError}
+				/>
+				<IconInputPassword
+					loginScreen
+					src={this.state.showPassword ? showPassword : hidePassword}
+					onClick={this.showPassword}
+				/>
+				{this.renderError()}
+			</InputBox>
+			<Button width='55%'>
+				Entrar
+			</Button>
+			<AltBox>
+				<Link onClick={this.handleCreate} >Criar nova conta</Link>
+				<Link>Recuperar Senha</Link>
+			</AltBox>
+		</InputContainer>
+		<LoginBox>
+			<Img src={imagemPrincpal} />
+			<TextBox>
+				<Text>
+					Defina seus filtros, receba contratos selecionados e esqueça como listas de mil itens.
+				</Text>
+				<Link onClick={this.handleCreate} color={'#AADF00'}>Criar nova conta</Link>
+			</TextBox>
+		</LoginBox>
+	</>
+
+	renderCreateAccount = () => (
+		<CreateContainer>
+			<LogoCreate src={logoW} />
+			<CreateBox onSubmit={this.createSubmit}>
+				<CreateTitle>
+					Criar Conta
+				</CreateTitle>
+				<InputBox width='100%'>
+					<Label>Nome</Label>
+					<Input
+						ref={(node) => { this.createNameRef = node; }}
+						required
+						autoFocus
+						placeholder={'Nome'}
+						error={this.props.error}
+						onChange={this.handleError}
+					/>
+				</InputBox>
+				<InputBox last width='100%'>
+					<Label>Email</Label>
+					<Input
+						ref={(node) => { this.createEmailRef = node; }}
+						type={'email'}
+						required
+						placeholder={'name@email.com'}
+						error={this.props.error}
+						onChange={this.handleError}
+					/>
+					{this.renderError()}
+				</InputBox>
+				<InputBox last width='100%'>
+					<Label>Senha</Label>
+					<Input
+						ref={(node) => { this.createPasswordRef = node; }}
+						type={this.state.showPassword ? 'password' : 'text'}
+						required
+						placeholder={'Digite sua senha'}
+						error={this.props.error}
+						onChange={this.handleError}
+					/>
+					<IconInputPassword
+						loginScreen
+						src={this.state.showPassword ? showPassword : hidePassword}
+						onClick={this.showPassword}
+					/>
+				</InputBox>
+				<TermsText>
+					Clique abaixo para concordar com nossos <strong>Termos de Serviço</strong> e se inscrever.
+				</TermsText>
+				<Button width='100%'>
+					Concordar e criar conta
+				</Button>
+			</CreateBox>
+		</CreateContainer>
+	)
+
+	renderCurrentScreen = () => {
+		switch (this.state.currentScreen) {
+		case 'login':
+			return this.renderLogin();
+		case 'create':
+			return this.renderCreateAccount();
+		case 'forgot':
+			return this.renderForgotPassword();
+		case 'password':
+			return this.renderResetPassword();
+		case 'invite':
+			return this.renderConfirmEmailMessage();
+		default:
+			return this.renderLogin();
+		}
+	}
+
 	render() {
 		return (
 			<Container>
-				<InputContainer onSubmit={this.handleSubmit}>
-					<Logo src={logo}/>
-					<InputBox>
-						<Label>E-mail</Label>
-						<Input
-							ref={(node) => { this.loginEmailRef = node; }}
-							required
-							autoFocus
-							type='email'
-							placeholder={'Digite sua senha e-mail'}
-							error={this.props.error}
-							onChange={this.handleError}
-						/>
-					</InputBox>
-					<InputBox last>
-						<Label>Senha</Label>
-						<Input
-							ref={(node) => { this.loginPasswordRef = node; }}
-							type={this.state.showPassword ? 'password' : 'text'}
-							required
-							placeholder={'Digite sua senha'}
-							error={this.props.error}
-							onChange={this.handleError}
-						/>
-						<IconInputPassword
-							loginScreen
-							src={this.state.showPassword ? showPassword : hidePassword}
-							onClick={this.showPassword}
-						/>
-						{this.renderError() }
-					</InputBox>
-					<Button>
-						Entrar
-					</Button>
-					<AltBox>
-						<Link>Criar nova conta</Link>
-						<Link>Recuperar Senha</Link>
-					</AltBox>
-				</InputContainer>
-				<LoginBox>
-					<Img src={imagemPrincpal} />
-					<TextBox>
-						<Text>
-							Defina seus filtros, receba contratos selecionados e esqueça como listas de mil itens.
-						</Text>
-						<Link color={'#AADF00'}>Criar nova conta</Link>
-					</TextBox>
-				</LoginBox>
+				{this.renderCurrentScreen()}
 			</Container>
 		);
 	}
