d45a2186c147ce40aa14e68cb514883a9cfc7e10
mymrtt <mymrtt@gmail.com>
2019-11-05 14:17:24 -0300

    Updates

---
+++ precache-manifest.c7d209d535f1a4ba38509a908fe0862c.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b151c63a9c0c530a07c8a536001a24",
+    "url": "/Petronect/index.html"
+  },
+  {
+    "revision": "75764ff16c335efea40d",
+    "url": "/Petronect/static/css/main.87ffdde6.chunk.css"
+  },
+  {
+    "revision": "f8766c37861722e95087",
+    "url": "/Petronect/static/js/2.9ea03965.chunk.js"
+  },
+  {
+    "revision": "75764ff16c335efea40d",
+    "url": "/Petronect/static/js/main.725bf0d5.chunk.js"
+  },
+  {
+    "revision": "86a61e37a012c9dd63b4",
+    "url": "/Petronect/static/js/runtime~main.077b1d89.js"
+  },
+  {
+    "revision": "5c61e86083c08749f2463b824263e18d",
+    "url": "/Petronect/static/media/Grupo-8105.5c61e860.svg"
+  },
+  {
+    "revision": "5e0d7b8e0ead104d458acd72fcb99af4",
+    "url": "/Petronect/static/media/LogoPNE.5e0d7b8e.png"
+  },
+  {
+    "revision": "4ffa926e8bc725a545d0a5b8417c1087",
+    "url": "/Petronect/static/media/anexar.4ffa926e.svg"
+  },
+  {
+    "revision": "c6ec2575fad12d8f1e5cd998fc71bb2c",
+    "url": "/Petronect/static/media/close-blue.c6ec2575.svg"
+  },
+  {
+    "revision": "a88e11535aebfc14931dee2e740cd400",
+    "url": "/Petronect/static/media/delete.a88e1153.svg"
+  },
+  {
+    "revision": "0b872c712a326ac030784c564b62c174",
+    "url": "/Petronect/static/media/dropdown.0b872c71.svg"
+  },
+  {
+    "revision": "3a11805d24eeaa37ea58bdeb22669d78",
+    "url": "/Petronect/static/media/estrela-cinza.3a11805d.svg"
+  },
+  {
+    "revision": "4b93e6c617d763ccce257e1392050d91",
+    "url": "/Petronect/static/media/estrela.4b93e6c6.svg"
+  },
+  {
+    "revision": "f29831e0c8cfcd63249b46ce38a0186f",
+    "url": "/Petronect/static/media/icon_menu-Relevancia.f29831e0.svg"
+  },
+  {
+    "revision": "5f5c99906de79fb93b5b17abf0ff58eb",
+    "url": "/Petronect/static/media/icon_menu-analise.5f5c9990.svg"
+  },
+  {
+    "revision": "d5e918d63f1127c346cf9baf4ad3c1cd",
+    "url": "/Petronect/static/media/icon_menu-geracaoDoc.d5e918d6.svg"
+  },
+  {
+    "revision": "1f2c87ab51021c661a8b3ebc1ede3415",
+    "url": "/Petronect/static/media/icon_menu-gestão.1f2c87ab.svg"
+  },
+  {
+    "revision": "5c4edcac7c64e674f9bcf8ad2f39ab5d",
+    "url": "/Petronect/static/media/icon_menu-historico.5c4edcac.svg"
+  },
+  {
+    "revision": "a458ddd6ea4b8c322f43904d45d2bb51",
+    "url": "/Petronect/static/media/icon_menu-hitorico.a458ddd6.svg"
+  },
+  {
+    "revision": "b8c8dca44fd6247277565c80033a12e7",
+    "url": "/Petronect/static/media/icon_menu-justificativa.b8c8dca4.svg"
+  },
+  {
+    "revision": "5ebbd991efcf34897043f7c615e793be",
+    "url": "/Petronect/static/media/icon_menu-notificação.5ebbd991.svg"
+  },
+  {
+    "revision": "7b2bd9d538f1c9369ed62b5920614114",
+    "url": "/Petronect/static/media/icon_menu.7b2bd9d5.svg"
+  },
+  {
+    "revision": "96af5c8fdc1e2e6ce571aa487b7465e2",
+    "url": "/Petronect/static/media/icon_menu_input.96af5c8f.svg"
+  },
+  {
+    "revision": "f95eae64c150b0aef03432210bbc41d5",
+    "url": "/Petronect/static/media/login-hide-password.f95eae64.svg"
+  },
+  {
+    "revision": "54d5a3d6ccea73de0100839e4239d2df",
+    "url": "/Petronect/static/media/login-show-password.54d5a3d6.svg"
+  },
+  {
+    "revision": "4972243e3e3c448dee681b890f69995a",
+    "url": "/Petronect/static/media/logoBranca.4972243e.svg"
+  },
+  {
+    "revision": "8601e100a078ec38e9ebf48140933c9e",
+    "url": "/Petronect/static/media/lupa.8601e100.svg"
+  },
+  {
+    "revision": "8a4fe04e7e63ad7b7c54c05f7b347367",
+    "url": "/Petronect/static/media/menu_hamburger_blue.8a4fe04e.svg"
+  },
+  {
+    "revision": "1106b9a108e4128b7b87d21e076e6dcf",
+    "url": "/Petronect/static/media/menu_hamburguer.1106b9a1.svg"
+  },
+  {
+    "revision": "6396510f6f95e354d28be2c166b82abf",
+    "url": "/Petronect/static/media/search.6396510f.svg"
+  }
+]);
